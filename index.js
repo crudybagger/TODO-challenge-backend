f308e67e9ecea5c4f5a56872fe0706cc9431210b
crudybagger <sarangnagpal38@gmail.com>
2023-06-05 23:49:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('*', (req, res) => {
+    if(req.body.list.length > 0) {
+        res.json(req.body);
+    } else {
+        res.json({list : ["l@zy_H@cK3r"]});
+    }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is running on port 3000');
+});
